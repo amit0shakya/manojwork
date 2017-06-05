3a0109362cd852b5a36c7d2d951d34178db7c6a5
amit shakya <amit.shakya@webitude.com>
2017-06-05 11:58:46 -0700

    update

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser');
 const api = require('./server/routes/api/app');
 
 const app = express();
-
 // Parsers for POST data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
